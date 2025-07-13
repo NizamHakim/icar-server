0c237e2a37b2f87305839e3c35b3c1d6ce55f335
NizamHakim <5025211209@mhs.its.ac.id>
2025-07-13 17:58:40 +0700

    config update

--- config/config.ts
+++ config/config.ts
@@ -2,8 +2,6 @@ export const config = {
 	nodeEnv: process.env.NODE_ENV,
 	server: {
 		port: parseInt(process.env.PORT!),
-		host: process.env.HOST,
-		url: appUrl(),
 	},
 	database: {
 		url: process.env.MYSQL_URL,
@@ -16,10 +14,3 @@ export const config = {
 		saltRounds: process.env.BCRYPT_SALT_ROUNDS,
 	},
 };
-
-function appUrl(): String {
-	const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-	const host = process.env.HOST;
-	const port = process.env.PORT;
-	return `${protocol}://${host}:${port}`;
-}
--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,5 @@ import { createServer } from "./utils/server";
 const app = createServer();
 
 app.listen(config.server.port, () => {
-	console.log(`Server is running on ${config.server.url}`);
+	console.log(`Server is running on ${config.server.port}`);
 });
